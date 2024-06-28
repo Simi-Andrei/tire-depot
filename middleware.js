5874644132abc49f1db776797cdec13d1c1075d0
Simi-Andrei <andy_andrei9572@yahoo.com>
2024-06-28 15:01:23 +0300

    next-auth dashboard

--- middleware.js
+++ middleware.js
@@ -2,7 +2,7 @@ export { default } from "next-auth/middleware";
 
 export const config = {
   matcher: [
-    "/dashboard",
+    // "/dashboard",
     // "/tires/:path*",
     // "/estimates/:path*",
     // "/entries/:path*",
